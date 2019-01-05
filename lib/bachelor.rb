bda6148d1bd4f75c1ff107730a07a4dae60ae2af
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-05 19:40:49 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -8,3 +8,53 @@ def get_first_name_of_season_winner(data, season)
     end
   end
 end
+
+def get_contestant_name(data, occupation)
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
+
+  def count_contestants_by_hometown(data, hometown)
+    counter = 0
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          counter += 1
+        end
+      end
+    end
+  end
+counter
+
+def get_occupation(data, hometown)
+  data.each do |season, array|
+    array.each do |hash|
+      hash.each do |k,v|
+        if v == hometown
+          return hash["occupation"]
+        end
+      end
+    end
+  end
+
+  def get_average_age_for_season(data, season)
+    age = 0
+count = 0
+  data[season].each do |hash|
+    hash.each do |k,v|
+      if k == "age"
+        count += 1
+        age += v.to_f
+      end
+    end
+  end
+
+   answer = (age/count).round
+return answer
