a9f23fbc7131cce5e7bd87cb1326eead011638a3
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-05 19:39:28 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,1 +1,10 @@
 def get_first_name_of_season_winner(data, season)
+  data[season].each do |people|
+
+   people.each do |k,v|
+    if v == "Winner"
+        full_name = people["name"]
+        return full_name.split(' ').first
+    end
+  end
+end
