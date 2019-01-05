13130af9f79f7729dce351b24ef5f61d0a1facfe
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-05 19:37:38 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -32,3 +32,20 @@ data = {
     }
   ]
 }
+def get_first_name_of_season_winner(data,season)
+ data[season.to_sym].each do |into_array|
+   if into_array[:status] =="Winner"
+     return into_array[:name].split(" ")[0]
+   end
+ end
+end
+
+def get_contestant_name(data, occupation)
+data.each do |season, season_array|
+  season_array.each do |into_array|
+    if into_array[:occupation] == occupation
+      return into_array[:name]
+      end
+    end
+  end
+end
